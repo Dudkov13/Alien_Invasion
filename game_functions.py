f7124499a00fb34bb96bae0a518304f259bec0d9
Jakub Dudek <65489788+Dudkov13@users.noreply.github.com>
2020-05-17 18:27:41 +0200

    Add files via upload

---
+++ game_functions.py
@@ -0,0 +1,118 @@
+import sys
+
+import pygame
+
+from bullet import Bullet
+
+from alien import Alien
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Respond to keypresses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key ==pygame.K_q:
+        sys.exit()
+
+def fire_bullet(ai_settings,screen, ship, bullets):
+    """Fire a bullet if limit not reached yet"""
+    #Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keyup_events(event, ship):
+    """Respond to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Respond to keypresses and mouse events"""
+    # Watch for keyboard and mouse events.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship, aliens, bullets):
+    """Update images on the screen and flip to the new screen"""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    aliens.draw(screen)
+
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+def update_bullets(aliens, bullets):
+    """Update position of bullets and get rid of old bullets"""
+    #Upate bullet position.
+    bullets.update()
+
+    #Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+    #Check for colisions and delete bullets and aliens.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determine number of aliens that fit in a row"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_alien_x = int(available_space_x / (2 * alien_width))
+    return number_alien_x
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """Create alien and place it in a row"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+def create_fleet(ai_settings, screen, aliens):
+    """Create a fleet of aliens"""
+    alien = Alien(ai_settings, screen)
+    number_alien_x = get_number_aliens_x(ai_settings, alien.rect.width)
+
+
+    #Create first row of aliens.
+    for alien_number in range(number_alien_x):
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+
+def check_fleet_edges(ai_settings, aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet`s direction"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings, aliens):
+    """
+    Check if the fleet is at an edge
+     and then update the postions of all aliens in the fleet.
+    """
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
